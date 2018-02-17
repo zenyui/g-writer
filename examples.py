acb50a8907d37f511c537c77c28dc6854c15ed73
Zen Yui <zenyui@gmail.com>
2018-02-16 23:16:33 -0500

    prepare sample messages

--- examples.py
+++ examples.py
@@ -5,40 +5,34 @@ import logging
 
 def cli():
     configure_logging()
+    logger = logging.getLogger()
+
     p = argparse.ArgumentParser()
 
-    p.add_argument('question', help='select 2, 3, or bonus')
+    p.add_argument('-c', help='path to ciphertext', dest='ciphertext', required=True)
+    p.add_argument('-p', help='(optional) path to plaintext', dest='plaintext')
+    p.add_argument('--show', help='output decrypted plaintext', dest='show_plain', action='store_true')
     a = p.parse_args()
 
-    if a.question=='3':
-        folder = './data/part_3'
+    with open(a.ciphertext,'r') as f:
+        ciphertext = f.read().strip()
 
-        with open(os.path.join(folder, 'ciphertext.txt')) as f:
-            ciphertext = f.read().strip()
-
+    plaintext = None
+    if a.plaintext:
         with open(os.path.join(folder, 'plaintext.txt')) as f:
             plaintext = f.read().strip()
 
-        success, gwriter_obj = gwriter.crack(
-            plaintext=plaintext,
-            ciphertext=ciphertext
-        )
+    success, gwriter_obj = gwriter.crack(
+        plaintext=plaintext,
+        ciphertext=ciphertext
+    )
 
-        if success:
-            print(gwriter_obj.to_str())
-
-    elif a.question in ('bonus','4'):
-        with open('./data/bonus/bonus_ciphertext.txt') as f:
-            ciphertext = f.read().strip()
-
-        success, gwriter_obj = gwriter.crack(ciphertext=ciphertext)
-
-        if success:
-            print(gwriter_obj.to_str())
-
-    else:
-        logging.error('Unknown argument {}'.format(a.question))
-
+    if success:
+        logger.info('success\n{}'.format(gwriter_obj.to_str()))
+        if a.show_plain:
+            logger.info('\n{s}\n\tBEGIN MESSAGES\n{s}\n'.format(s='*'*50))
+            decrypted = gwriter_obj.decrypt(ciphertext.splitlines())
+            print('\n'.join(decrypted))
 
 def configure_logging():
     root = logging.getLogger()
